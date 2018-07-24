6a84f3da88e7ca7229051ecec6d3ed54a3846e36
Trumpets <operations@trumpetstechnologies.com>
2018-07-24 23:48:25 +0530

    wiki-rate

--- resources/views/wikis/show2.blade.php
+++ resources/views/wikis/show2.blade.php
@@ -8,7 +8,7 @@
 			<h2 class="text-black mb-35 font-semibold text-3xl border-t-0 border-x-0 border-b border-black pb-3">{{ $wiki->title }}</h2>
 			<latex-editor source-url="/wiki/{{ $wiki->id }}/body"></latex-editor>
 
-            <div class="container mt-45 wiki-rate-box w-full shadow-lg py-35 px-20 md:mx-auto bg-white border-2 border-x-0 border-b-0 ">
+            <div class="container mt-45 wiki-rate-box w-full shadow-lg py-35 px-20 md:mx-auto bg-white border-4 border-brand border-l-0 border-b-0 border-r-0">
 
                     <div class="flex">
                         <span class="text-yellow text-2xl flex-1 tracking-wide mt-2"><span class="text-black font-semibold">Rate this wiki :</span> <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i></span>
