d5cd0c50b4b6a6fe3d0cae8578273edc08f86b7b
Deep Thakkar <d33pthakkar@gmail.com>
2019-03-31 23:04:51 +0530

    changes

--- resources/views/pages/terms.blade.php
+++ resources/views/pages/terms.blade.php
@@ -1,17 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-	
-	<h2>About Us</h2>
+<div class="container mx-auto text-xl tracking-wide leading-normal mb-6">
 
-	<div>
+	<h2 class="text-3xl font-weight-bold my-6">About Us</h2>
 
+	<p>Aegis Academy is an attempt to provide a high quality education particularly for the preparation of the entrance exams for the undergraduate courses in ISI (Indian Statistical Institute) and CMI (Chennai Mathematical Institute). Our courses are also helpful in regional and national level maths olympiads. Our courses are also helpful in IIT-JEE and KVPY preparation.</p> 
 
-	<p>Aegis Academy is an attempt to provide a high quality education particularly for the preparation of the entrance exams for the undergraduate courses in ISI (Indian Statistical Institute) and CMI (Chennai Mathematical Institute). Our courses are also helpful in regional and national level maths olympiads. Our courses are also helpful in IIT-JEE and KVPY preparation.</p>
+	<br>
 
-	<p>There are plenty of offline and online coaching available for the preparation of entrance exams (especially for IIT-JEE) in India. But most of them are seriously lacking in quality training. They teach more intuitively than rigorously. Although our intuition is an asset in mathematics but it cannot be a substitute of rigour. Another serious problem is that they are unable to develop problem solving skills in aspirants. When we solve a problem by heavily relying on the intuition rather than achieving the answer by logical deductions from theorems or proven results at each step, the real purpose of studying mathematics, developing a problem solving skill which enables us to unfold the problem logically and beautifully and hence we become able to solve real-world critical problems, is defeated.</p>
-
+	<p>There are plenty of offline and online coaching available for the preparation of entrance exams (especially for IIT-JEE) in India. But most of them are seriously lacking in quality training. They teach more intuitively than rigorously. Although our intuition is an asset in mathematics but it cannot be a substitute of rigour. Another serious problem is that they are unable to develop problem solving skills in aspirants. When we solve a problem by heavily relying on the intuition rather than achieving the answer by logical deductions from theorems or proven results at each step, the real purpose of studying mathematics, developing a problem solving skill which enables us to unfold the problem logically and beautifully and hence we become able to solve real-world critical problems, is defeated.</p> 
+	<br>
 	<p>Enjoy learning and get strength.</p>
-	</div>	
 
+</div>
 @endsection
