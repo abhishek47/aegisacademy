2509e964d70b2cb09750fafdfa90858fde0f50f5
Deep Thakkar <d33pthakkar@gmail.com>
2019-03-31 23:19:26 +0530

    changes

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -6,7 +6,7 @@
 
 		<h2 class="text-3xl font-weight-bold my-6">About Us</h2>
 
-		<div class="flex justify-between mb-6">
+		<div class="flex justify-between text-center mb-6">
 			<div>
 				<i class="fas fa-phone text-3xl text-grey-darkest"></i>
 				<h4 class="font-bold">Phone</h4>
@@ -18,7 +18,7 @@
 				<p class="text-grey-darker">Nashik, Maharashtra, India</p>
 			</div>
 			<div>
-				<i class="fas fa-mail text-3xl text-grey-darkest"></i>
+				<i class="fas fa-envelope text-3xl text-grey-darkest"></i>
 				<h4 class="font-bold">Email</h4>
 				<p class="text-grey-darker">info@aegisacademy.co.in</p>
 			</div>
