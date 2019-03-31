2d5f8dcedb32ef20c7b5bc44c531040ce4a28514
Deep Thakkar <d33pthakkar@gmail.com>
2019-03-31 23:17:33 +0530

    changes

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -2,20 +2,23 @@
 
 @section('content')
 
-	<div class="container mx-auto text-xl tracking-wide leading-normal mb-6">
+	<div class="container mx-auto text-xl tracking-wide leading-normal mb-6" style="height: 100vh;">
 
 		<h2 class="text-3xl font-weight-bold my-6">About Us</h2>
 
-		<div class="flex">
+		<div class="flex justify-between mb-6">
 			<div>
+				<i class="fas fa-phone text-3xl text-grey-darkest"></i>
 				<h4 class="font-bold">Phone</h4>
 				<p class="text-grey-darker">(+91) 8800106866</p>
 			</div>
 			<div>
+				<i class="fas fa-map-marker text-3xl text-grey-darkest"></i>
 				<h4 class="font-bold">Address</h4>
 				<p class="text-grey-darker">Nashik, Maharashtra, India</p>
 			</div>
 			<div>
+				<i class="fas fa-mail text-3xl text-grey-darkest"></i>
 				<h4 class="font-bold">Email</h4>
 				<p class="text-grey-darker">info@aegisacademy.co.in</p>
 			</div>
