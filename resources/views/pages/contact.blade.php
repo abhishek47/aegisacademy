9e53b2ff276557dfa2285636c0e7c34372487c24
Deep Thakkar <d33pthakkar@gmail.com>
2019-03-31 23:21:03 +0530

    contact

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -4,7 +4,9 @@
 
 	<div class="container mx-auto text-xl tracking-wide leading-normal mb-6" style="height: 100vh;">
 
-		<h2 class="text-3xl font-weight-bold my-6">About Us</h2>
+		<h2 class="text-3xl font-weight-bold my-6">Contact Us</h2>
+
+		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d59990.65887208493!2d73.76846144556879!3d19.991040271901912!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bddd290b09914b3%3A0xcb07845d9d28215c!2sNashik%2C+Maharashtra!5e0!3m2!1sen!2sin!4v1508123275428" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
 
 		<div class="flex justify-between text-center mb-6">
 			<div>
