ea81d1a4312becf3f75f9c7973bf233be02f4ea9
Deep Thakkar <d33pthakkar@gmail.com>
2019-03-31 23:21:57 +0530

    map

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -6,9 +6,9 @@
 
 		<h2 class="text-3xl font-weight-bold my-6">Contact Us</h2>
 
-		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d59990.65887208493!2d73.76846144556879!3d19.991040271901912!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bddd290b09914b3%3A0xcb07845d9d28215c!2sNashik%2C+Maharashtra!5e0!3m2!1sen!2sin!4v1508123275428" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d59990.65887208493!2d73.76846144556879!3d19.991040271901912!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bddd290b09914b3%3A0xcb07845d9d28215c!2sNashik%2C+Maharashtra!5e0!3m2!1sen!2sin!4v1508123275428" width="100%" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
 
-		<div class="flex justify-between text-center mb-6">
+		<div class="flex justify-between text-center my-6">
 			<div>
 				<i class="fas fa-phone text-3xl text-grey-darkest"></i>
 				<h4 class="font-bold">Phone</h4>
@@ -17,7 +17,7 @@
 			<div>
 				<i class="fas fa-map-marker text-3xl text-grey-darkest"></i>
 				<h4 class="font-bold">Address</h4>
-				<p class="text-grey-darker">Nashik, Maharashtra, India</p>
+				<p class="text-grey-darker">Luckhnow, Maharashtra, India</p>
 			</div>
 			<div>
 				<i class="fas fa-envelope text-3xl text-grey-darkest"></i>
