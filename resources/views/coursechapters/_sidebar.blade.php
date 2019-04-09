ef3d88be6b7ef52e9242a47bbe9e6252e69cc44e
trumpetstech <work.trumpets@gmail.com>
2019-04-09 12:53:52 +0530

    fix sidebar size

--- resources/views/coursechapters/_sidebar.blade.php
+++ resources/views/coursechapters/_sidebar.blade.php
@@ -1,4 +1,4 @@
-<div  v-if="showSidebar" class="bg-blue-lightest p-4 pr-60" style="min-height: 100vh;max-width: 450px;width: 450px;">
+<div  v-if="showSidebar" class="bg-blue-lightest p-4 pr-60" style="min-height: 100vh;max-width: 315px;width: 315px !important;">
     <div class="flex flex-col p-4 pl-0 pr-6 mx-3">
       {{--   <button class="shadow bg-transparent hover:bg-green border-2 border-green rounded text-green hover:text-white p-3 px-6 w-full font-semibold tracking-wide">
         <i class="fa fa-star mr-2"></i> Bookmark
